3b2aff9fa1c41f8df38adfe021a172894042d6b0
shakirat <shakiratganiyy@gmail.com>
2023-05-24 12:53:46 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This is coded by Shakirat Adeyemi and it is{" "}
-        <a
-          href="https://github.com/shakiratkemi/my-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced.
-        </a>
-      </footer>
-      ;
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This is coded by Shakirat Adeyemi and it is{" "}
+          <a
+            href="https://github.com/shakiratkemi/my-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced{" "}
+          </a>
+          on GitHub.
+        </footer>
+      </div>
     </div>
   );
 }
