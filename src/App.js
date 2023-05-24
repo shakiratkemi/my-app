1e37d8d16ee654c8d9828c9f02f091c67c3d6108
shakirat <shakiratganiyy@gmail.com>
2023-05-24 11:40:49 +0100

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This is coded by Shakirat Adeyemi and it is{" "}
+        <a
+          href="https://github.com/shakiratkemi/my-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced.
+        </a>
+      </footer>
+      ;
     </div>
   );
 }
-<footer>
-  This is coded by Shakirat Adeyemi and it is
-  <a href="" target="_blank" rel="noopener noreferrer">
-    open-sourced.
-  </a>
-</footer>;
 export default App;
