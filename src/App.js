502c0d13c928a613dd7fdb334c87c82af49f2d32
shakirat <shakiratganiyy@gmail.com>
2023-05-27 23:31:36 +0100

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Weather from "./Weather";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This is coded by Shakirat Adeyemi and it is{" "}
           <a
